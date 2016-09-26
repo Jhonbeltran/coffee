9fdd69dd720d736b92e14655a3eacca1fbe46446
Jhonbeltran <jbeltranleon@gmail.com>
2016-09-26 15:52:03 -0500

    creo base pantalla de inicio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,45 +6,45 @@
         <div class="col s12 l4">
           <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator" src="http://www.cafespanchito.com/wp-content/uploads/2014/03/cafecanela.jpg">
+              <img class="activator" src="http://www.revistasumma.com/wp-content/uploads/2015/04/Caf%C3%A91.jpg">
             </div>
             <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-              <p><a href="#">This is a link</a></p>
+              <span class="card-title activator grey-text text-darken-4">Ingresar Datos<i class="material-icons right">more_vert</i></span>
+              <p><a href="#">Iniciar</a></p>
             </div>
             <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+              <span class="card-title grey-text text-darken-4">Ingresar Datos<i class="material-icons right">close</i></span>
+              <p>Acá podrás ingresar los datos del cultivo.</p>
             </div>
           </div>
         </div>
         <div class="col s12 l4">
           <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator" src="http://www.cafespanchito.com/wp-content/uploads/2014/03/cafecanela.jpg">
+              <img class="activator" src="https://static.pexels.com/photos/6347/coffee-cup-working-happy.jpg">
             </div>
             <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-              <p><a href="#">This is a link</a></p>
+              <span class="card-title activator grey-text text-darken-4">Modificar<i class="material-icons right">more_vert</i></span>
+              <p><a href="#">Editar</a></p>
             </div>
             <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+              <span class="card-title grey-text text-darken-4">Modificar<i class="material-icons right">close</i></span>
+              <p>Actualiza la base de datos de Semillas.</p>
             </div>
           </div>
         </div>
         <div class="col s12 l4">
           <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator" src="http://www.cafespanchito.com/wp-content/uploads/2014/03/cafecanela.jpg">
+              <img class="activator" src="http://sawadacoffee.com/wp-content/uploads/Sawada-Coffee-10DEC2015-003.jpg">
             </div>
             <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-              <p><a href="#">This is a link</a></p>
+              <span class="card-title activator grey-text text-darken-4">Registro<i class="material-icons right">more_vert</i></span>
+              <p><a href="#">Ver</a></p>
             </div>
             <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+              <span class="card-title grey-text text-darken-4">Registro<i class="material-icons right">close</i></span>
+              <p>Consulta los diferentes cultivos almacenados.</p>
             </div>
           </div>
         </div>
