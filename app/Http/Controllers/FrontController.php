6d9dcd6f989b210d88ff19db3a643a2437cbe3ad
Jhonbeltran <jbeltranleon@gmail.com>
2016-09-25 21:29:10 -0500

    Creo controller FrontController: php artisan make:controller FrontController configuro la ruta con Route::get... creo las funciones index y contacto en el controlador

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace coffee\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use coffee\Http\Requests;
+
+class FrontController extends Controller
+{
+    /**
+     *Display a listing of the resourse.
+     *
+     *@return Response
+     */
+    public function index(){
+
+    }
+
+    public function contacto(){
+
+    }
+}
