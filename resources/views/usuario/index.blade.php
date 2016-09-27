28041d96581df0662a0cc6ac306db1ab35226d78
Jhonbeltran <jbeltranleon@gmail.com>
2016-09-26 21:17:52 -0500

    Agrego boton en la tabla para editar los usuarios registrados, uso un linl_to_route de laravelcollective

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -14,13 +14,15 @@
 		<thead>
 			<th>Nombre</th>
 			<th>Correo</th>
-			<th>Oprecion</th>
+			<th>Operación</th>
 		</thead>
 		@foreach($users as $user)
 		<tbody>
 			<td>{{$user->name}}</td>
 			<td>{{$user->email}}</td>
-			<td></td>
+			<td>
+				{!!link_to_route('usuario.edit', $title = 'Editar', $parameters = $user->id, $attributes = ['class'=>'btn btn-flat waves-effect waves-red'])!!}
+			</td>
 		</tbody>
 		@endforeach
 	</table>
