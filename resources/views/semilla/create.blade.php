68b1f02c2253b2af27d26810ad32c16b639bf5fc
Jhonbeltran <jbeltranleon@gmail.com>
2016-09-28 15:29:12 -0500

    agrego name a form de semilla y cambio el boton por un input

--- resources/views/semilla/create.blade.php
+++ resources/views/semilla/create.blade.php
@@ -11,13 +11,13 @@
 			<div class="row">
 				<div class="input-field col s12 center">
 		            <i class="material-icons prefix">invert_colors</i>
-		            <input id="icon_prefix" type="text" class="validate"/>
+		            <input id="icon_prefix" type="text" class="validate" name="name" />
 		            <label for="icon_prefix">Nombre</label>
 		        </div>
 			</div>
 			<div class="row center">
 	            <div class="col s12">
-	                <button class="btn deep-orange">Agregar</button>
+	                <input type="submit" class="btn deep-orange" value="Agregar" />
 	            </div>
 	        </div>
 		</form>
