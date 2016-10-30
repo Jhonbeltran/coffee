60e7e13e362088e9519b244577b7aee2f3728be2
Jhonbeltran <jbeltranleon@gmail.com>
2016-10-30 11:12:57 -0500

    agrego controllador Producer

---
+++ app/Http/Controllers/Producer.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Coffee\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Coffee\Http\Requests;
+
+use Coffee\Productor;
+
+class Producer extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productors = Productor::All();
+        return view('productor.index', compact('productors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('productor.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Productor::create(['nombre' => $request['nombre']]);
+        return redirect('/productor')->with('message', 'store')
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $productor = Productor::find($id);
+        return view('productor.edit',['productor'=>$productor]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $productor = Productor::find($id);
+        $productor->fill($request->all());
+        $productor->save();
+        return redirect('/productor')->with('message', 'edit');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Productor::destroy($id);
+        return redirect('/productor')->with('message', 'delete');
+    }
+}
