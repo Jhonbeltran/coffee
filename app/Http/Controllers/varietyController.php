82b88285cd87b0ce0521be504b43559957692634
Jhonbeltran <jbeltranleon@gmail.com>
2016-10-31 14:56:23 -0500

    agrego varietyController

---
+++ app/Http/Controllers/varietyController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Coffee\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Coffee\Http\Requests;
+
+use Coffee\Variedad;
+
+class varietyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $variedads = Variedad::All();
+        return view('variedad.index', compact('variedads'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('variedad.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Variedad::create(['nombre' => $request['nombre']]);
+        return redirect('variedad')->with('message', 'store');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $variedad = Variedad::find($id);
+        return view('variedad.edit',['variedad'=>$variedad]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $variedad = Variedad::find($id);
+        $variedad->fill($request->all());
+        $variedad->save();
+        return redirect('variedad')->with('message', 'edit');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Variedad::destroy($id);
+        return redirect('variedad')->with('message', 'delete');
+    }
+}
