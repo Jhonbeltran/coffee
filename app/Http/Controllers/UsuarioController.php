793ddbbbcf4ceae1bf2987aa1b43c004dc4685aa
Jhonbeltran <jbeltranleon@gmail.com>
2016-11-16 16:38:57 -0500

    No es la mejor forma de subir algo a producción pero es la unica que me pude idear por ahora, comento el middleware en usuario para poder crear el usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -11,9 +11,9 @@ use Coffee\User;
 
 class UsuarioController extends Controller
 {
-    public function __construct(){
+    /*public function __construct(){
         $this->middleware('auth');
-    }
+    }*/
     /**
      * Display a listing of the resource.
      *
